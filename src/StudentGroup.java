d0d0f6f506a0e40aa2e1d93b539171a1cb51a43b
Hemachand Gogula <31966509+hemachandgogula@users.noreply.github.com>
2017-09-16 12:41:56 +0530

    padwanscomit6

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -26,63 +26,118 @@ public class StudentGroup implements StudentArrayOperation {
 	//@Override
 	public Student[] getStudents() {
 		// Add your implementation here
-			if(length==0)
-			{
-		throw new IllegalArgumentException("IllegalArgumentException");
-			}
-			else
-			return student[0];
+			return students;
 	}
 
 	//@Override
 	public void setStudents(Student[] students) {
 		// Add your implementation here
+		if(students==null)
+		throw new IllegalArgumentException("Invalid operation);
+		else
+		Student[] students=students[];
 	}
 
 	//@Override
 	public Student getStudent(int index) {
 		// Add your implementation here
-		return null;
+		if(index<0||index>students.length())
+		{
+		
+		throw new IllegalArgumentException("Invalid index);
+		}
+		return students[index];
 	}
 
 	//@Override
 	public void setStudent(Student student, int index) {
 		// Add your implementation here
+		if(students[index]==null)
+		throw new IllegalArgumentException("Invalid operation");
+		else if(index<0||index>students.length())
+		throw new IllegalArgumentException("Invalid length");
+		else
+		students[index]=student;
 	}
 
 	//@Override
 	public void addFirst(Student student) {
 		// Add your implementation here
+		if(students[index]==null)
+		throw new IllegalArgumentException("Invalid operation");
+		else
+		students[0]=student;
+		
 	}
 
 	//@Override
 	public void addLast(Student student) {
 		// Add your implementation here
+		if(students==null)
+		throw new IllegalArgumentException("Invalid operation");
+		else
+		students[students.length()+1]=student;
 	}
 
 	//@Override
 	public void add(Student student, int index) {
 		// Add your implementation here
+		if(students[index]==null)
+		throw new IllegalArgumentException("Invalid operation");
+		else if(index<0||index>students.length())
+		throw new IllegalArgumentException("Invalid length");
+		students[students.length()+1]=student;
 	}
 
 	//@Override
 	public void remove(int index) {
 		// Add your implementation here
+	if(index<0||index>students.length())
+		throw new IllegalArgumentException("Invalid length");
+	else
+	{
+	int i=index;
+	while(i<students.lenth())
+	{
+	students[i]=student[i+1];
+	i++;
+	}	
+	
 	}
 
 	//@Override
 	public void remove(Student student) {
 		// Add your implementation here
+		if(student==null)
+		throw new IllegalArgumentException("Invalid operation");
+		else
+		{
+			int i=0;
+		while(students[i]!=student)
+		{
+			i++;
+		}
+		while(i<students.length())
+		{	
+			student[i]=student[i+1];
+			i++;
+		}
 	}
-
 	//@Override
 	public void removeFromIndex(int index) {
 		// Add your implementation here
+	if(index<0||index>students.length())
+		throw new IllegalArgumentException("Invalid length");
+		students=ArrayUtils.remove(students,index);
 	}
 
 	//@Override
-	public void removeFromElement(Student student) {
+	public void removeFromElement(Student student) 
+	{
+	if(student==null)
+		throw new IllegalArgumentException("Invalid operation");
 		// Add your implementation here
+		
 	}
 
 	//@Override
